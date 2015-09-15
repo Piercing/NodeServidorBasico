0228f4c74981362d992c9240311da252e5f2dc76
Piercing <merlosalbarracin@gmail.com>
2015-09-16 00:11:48 +0200

    Creo 01_servidor_basico

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-/**
- * Created by macbookpro on 15/9/15.
- */
+"use strict"var http = require('http');var server = http.createServer(function (request, response) {    response.writeHead(200, {'Content-type': 'text/html'});    response.end('<style>html {background-image: url("http://bit.do/wakeupneo")</style>');});server.listen(1337, '127.0.0.1');console.log('Servidor arrancado en http://127.0.0.1:1337');
